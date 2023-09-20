0210ca73aad66cffc39e429a2664ba955c63b91b
“laozz” <piganof@gmail.com>
2023-09-20 19:00:00 +0800

    第五章 Jar打包部署、War打包部署

--- src/main/java/com/example/javascriptbootdome01/JavaScriptBootDome01Application.java
+++ src/main/java/com/example/javascriptbootdome01/JavaScriptBootDome01Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication//用注解标识的类作为springboot的入口类
 @ServletComponentScan//开启基于注解方式的Servlet组件扫描支持
-@ImportResource("classpath:xmlpropertoes.xml")//加载自定义xml配置文件位置
+@ImportResource("classpath:xmlpropertoes.xml")//加载自定义xml配置文件位置11
 public class JavaScriptBootDome01Application extends SpringBootServletInitializer {
     //程序主类继承 SpringBootServletInitializer，并重写configure（）方法
     @Override
