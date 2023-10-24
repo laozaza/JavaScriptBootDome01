d5d709d26b0bced4f141ce3aac6e01aa0e17e5e4
laozaza <piganof@gmail.com>
2023-10-24 17:25:55 +0800

    第9章 同步和异步任务

--- src/main/java/com/example/javascriptbootdome01/AsyncAppication.java
+++ src/main/java/com/example/javascriptbootdome01/AsyncAppication.java
@@ -1,24 +1,24 @@
-//package com.example.javascriptbootdome01;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//
-///**
-// * @BelongsProject: JavaScriptBootDome01  //项目名
-// * @BelongsPackage: com.example.javascriptbootdome01  //包名
-// * @ClassName AsyncAppication                //类名
-// * @Author: laozaza                   //作者
-// * @CreateTime: 2023-10-23  19:44  //时间
-// **/
-//@EnableAsync// 启用异步方法支持
-//@EnableScheduling//启用定时任务支持
-//@SpringBootApplication
-//public class AsyncAppication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(AsyncAppication.class, args);
-//    }
-//
-//
-//}
+package com.example.javascriptbootdome01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @BelongsProject: JavaScriptBootDome01  //项目名
+ * @BelongsPackage: com.example.javascriptbootdome01  //包名
+ * @ClassName AsyncAppication                //类名
+ * @Author: laozaza                   //作者
+ * @CreateTime: 2023-10-23  19:44  //时间
+ **/
+@EnableAsync// 启用异步方法支持
+@EnableScheduling//启用定时任务支持
+@SpringBootApplication
+public class AsyncAppication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncAppication.class, args);
+    }
+
+
+}
